bbda89db9f722e3c6acfcac5aeb2c8e62f43dd85
Edgar Pech Garcia <71201852+Nfsedg@users.noreply.github.com>
2023-10-14 20:49:27 -0500

    Add contacto view

---
+++ 24CV_WEB/Controllers/InformacionController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using _24CV_WEB.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace _24CV_WEB.Controllers
+{
+    public class InformacionController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult EnviarInformacion(InformacionViewModel model)
+        {
+            TempData["Comentario"] = model.Comentario;
+            TempData["Email"] = model.Email;
+
+            return View("Index", model);
+
+        }
+        public bool SendEmail(string email, string comentario)
+        {
+            MailMessage mail = new MailMessage();
+
+            SmtpClient smtp = new SmtpClient("mail.shapp.mx", 587);
+
+            smtp.EnableSsl = true;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("moises.puc@shapp.mx", "Dhaserck_999");
+            mail.From = new MailAddress("moises.puc@shapp.mx", "Administrador");
+            mail.To.Add(email);
+            mail.Subject = "Notificación de contacto.";
+            mail.IsBodyHtml = true;
+            mail.Body = $"Se ha recibido información del correo <h1>{email}</h1> <br/><p>{comentario}</p>";
+
+            smtp.Send(mail);
+
+            return true;
+        }
+    }
+}
+
---
+++ 24CV_WEB/Models/InformacionViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace _24CV_WEB.Models
+{
+	public class InformacionViewModel
+	{
+		public string Nombre { set; get; }
+		public string Apellidos { set; get; }
+		public string Email { set; get; }
+		public DateTime FechaNac { set; get; }
+		public string Turno { set; get; }
+		public string Comentario { set; get; }
+	}
+}
+
